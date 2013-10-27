e4307dfa2ff7cba78a811dc7374ea0ff238b6d21
mitch cail <mitch.cail@gmail.com>
2013-10-27 17:02:06 -0300

    intitial commit. also added rspec and capybabra gems for testing

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,29 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
+SampleApp::Application.config.secret_key_base = secure_token
+
+TwitterApp::Application.config.secret_key_base = '3dd48df1f167bfc4f281ab74d18a824801122f834ab51adf15d50f2a69b72b893e5774de23aaba81fa8815e051e9299dc527e156c5882797ae8c40384b1a52e5'
